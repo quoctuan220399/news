a7b222597c61692dd36b87091df8efd6e093bf26
unknown <tuanphamtran99@gmail.com>
2020-04-10 10:28:55 +0700

    transg vchu covid

---
+++ js/bai4.js
@@ -0,0 +1,122 @@
+$(document).ready(function () {
+    
+    $('ul.list li.apoi').click(function(){
+
+        $('.form-black').addClass('hien');
+        $('.table-form').addClass('xuong');
+        $('html').css({'overflow-y':'hidden'})
+    });
+
+    $('.table-form i.fa.fa-times').click(function(){
+
+        $('.form-black').removeClass('hien');
+        $('.table-form').removeClass('xuong');
+        $('html').css({'overflow-y':'scroll'})
+    });
+
+    $('.arr-l').click(function(){
+
+        var next = $('.active').next();
+        if(next.length === 1){
+        $('.active').addClass('out-left')
+        next.addClass('active1'); 
+        setTimeout(function(){
+
+            $('.out-left').removeClass('out-left');
+            $('.active').removeClass('active');
+        },500);
+        setTimeout(function(){
+            $('.active1').addClass('active');
+            $('.active1').removeClass('active1');
+        },500);}
+
+        else{
+            $('.active').addClass('out-left')
+            $('ul.slideshow li.slide:first-child').addClass('active1'); 
+            setTimeout(function(){
+    
+                $('.out-left').removeClass('out-left');
+                $('.active').removeClass('active');
+            },500);
+            setTimeout(function(){
+                $('.active1').addClass('active');
+                $('.active1').removeClass('active1');
+            },500);}
+    });
+
+    $('.arr-r').click(function(){
+
+        var prev = $('.active').prev();
+        if(prev.length === 1){
+        $('.active').addClass('out-left')
+        prev.addClass('active1'); 
+        setTimeout(function(){
+
+            $('.out-left').removeClass('out-left');
+            $('.active').removeClass('active');
+        },500);
+        setTimeout(function(){
+            $('.active1').addClass('active');
+            $('.active1').removeClass('active1');
+        },500);}
+
+        else{
+            $('.active').addClass('out-left')
+            $('ul.slideshow li.slide:last-child').addClass('active1'); 
+            setTimeout(function(){
+    
+                $('.out-left').removeClass('out-left');
+                $('.active').removeClass('active');
+            },500);
+            setTimeout(function(){
+                $('.active1').addClass('active');
+                $('.active1').removeClass('active1');
+            },500);}
+    });
+
+    setInterval(function(){
+        var next = $('.active').next();
+        if(next.length === 1){
+        $('.active').addClass('out-left')
+        next.addClass('active1'); 
+        setTimeout(function(){
+
+            $('.out-left').removeClass('out-left');
+            $('.active').removeClass('active');
+        },500);
+        setTimeout(function(){
+            $('.active1').addClass('active');
+            $('.active1').removeClass('active1');
+        },500);}
+
+        else{
+            $('.active').addClass('out-left')
+            $('ul.slideshow li.slide:first-child').addClass('active1'); 
+            setTimeout(function(){
+    
+                $('.out-left').removeClass('out-left');
+                $('.active').removeClass('active');
+            },500);
+            setTimeout(function(){
+                $('.active1').addClass('active');
+                $('.active1').removeClass('active1');
+            },500);}
+    },5000);
+
+    var vt1 = $('.slider').offset().top;
+    $(window).scroll(function () { 
+        
+        var scr = $('html').scrollTop();
+        console.log(scr);
+        console.log(vt1);
+        if(scr >= vt1){
+
+            $('header').addClass('gan');
+        }
+
+        else{
+
+            $('header').removeClass('gan');
+        }
+    });
+});
